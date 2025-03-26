e6988a2b803ef48decde602033ae0fbeb87be442
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-03-26 16:39:34 +0100

    Release: 2.0.1

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -9,7 +9,7 @@ const releaseConfig: ReleaseConfig = {
       build: true,
       test: false,
       release: {
-        version: "2.0.0",
+        version: "2.0.1",
         versionBumper: [
           {
             filePath: "src/package/constants.ts",
--- src/package/constants.ts
+++ src/package/constants.ts
@@ -1,2 +1,2 @@
-export const version = "2.0.0";
+export const version = "2.0.1";
 export const integrationType = `react-${version}`;
