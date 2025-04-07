b25c3b7c4dfc9fdbd4de5cd575424b0082e0931b
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-04-07 12:41:42 +0200

    fix linting errors

--- test-next/src/app/layout.tsx
+++ test-next/src/app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from "next";
 import "./globals.css";
 
+import type { Metadata } from "next";
+import type { JSX } from "react";
+import React from "react";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -11,13 +13,10 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>): JSX.Element {
   return (
     <html lang="en">
-      <body
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- test-next/src/app/page.tsx
+++ test-next/src/app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import type { JSX } from "react";
 import { SovendusThankyouPageReact } from "sovendus-integration-react";
-import { CountryCodes, SettingsType, Versions } from "sovendus-integration-types";
+import {
+  CountryCodes,
+  SettingsType,
+  Versions,
+} from "sovendus-integration-types";
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <SovendusThankyouPageReact
@@ -12,9 +17,9 @@ export default function Home() {
             settingType: SettingsType.SIMPLE,
             simple: {
               isEnabled: true,
-              trafficSourceNumber: "4704" ,
-              trafficMediumNumber:"5",
-              iframeContainerQuerySelector: undefined
+              trafficSourceNumber: "4704",
+              trafficMediumNumber: "5",
+              iframeContainerQuerySelector: undefined,
             },
             cookieTracking: true,
           },
@@ -29,7 +34,6 @@ export default function Home() {
           version: Versions.THREE,
         }}
         customerData={{
-
           consumerCountry: CountryCodes.DE,
         }}
         orderData={{
